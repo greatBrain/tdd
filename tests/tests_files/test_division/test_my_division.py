2bb3e95dd584d191a5d71ecab244aec11f04b58c
greatbrain <erickfania@gmail.com>
2023-06-20 00:25:23 -0400

    test mathematical functions

---
+++ tests/tests_files/test_division/test_my_division.py
@@ -0,0 +1,5 @@
+from calcs.my_division import divide
+import pytest
+
+def test_divide():
+    assert divide(15,3) == 5
---
+++ tests/tests_files/test_sum/test_my_sum.py
@@ -0,0 +1,5 @@
+from calcs.my_sum import my_sum
+
+#Testing the function my_sum
+def test_my_sum():
+    assert my_sum(2,2) == 4
